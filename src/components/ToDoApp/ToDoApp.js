56a90d5ce8e9ec217e61323e4d5499f27d574036
Ayşenur Ada <75562628+aysenurada@users.noreply.github.com>
2024-06-15 10:12:41 +0300

    fixed scroll and background image issues, added favicon

--- src/components/ToDoApp/ToDoApp.js
+++ src/components/ToDoApp/ToDoApp.js
@@ -1,10 +1,10 @@
+import { useContext } from 'react';
+import { ToDoContext } from '../../store/ToDoContext';
 import Title from '../Title/Title';
 import AddItem from '../AddItem/AddItem';
 import List from '../List/List';
 import Subtitle from '../Subtitle/Subtitle';
 import './ToDoApp.css';
-import { useContext } from 'react';
-import { ToDoContext } from '../../store/ToDoContext';
 
 const ToDoApp = () => {
 
